a7045279893f61b0fba72e6002bbb3c6dc469fd3
Jacob Sun <jacobsun000@gmail.com>
2025-01-17 13:53:40 -0600

    fix: Placeholder for resume

--- src/components/page-ui/LandingPage.tsx
+++ src/components/page-ui/LandingPage.tsx
@@ -36,7 +36,7 @@ export function LandingPage() {
         <p className="text-5xl text-neutral-600 dark:text-neutral-200 sm:text-5xl">Jacob Sun</p>
         <TypewriterEffectSmooth words={words} />
         <div className="z-30 flex flex-col items-center space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-          <Link href="https://drive.google.com/file/d/1xHPigW5eJQSCCgx3VBFyH5E0y_wwOdDN/view?usp=sharing">
+          <Link href="#TODO">
             <Button>Download CV</Button>
           </Link>
           <Link href="#contact-me">
